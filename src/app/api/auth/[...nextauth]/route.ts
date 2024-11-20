555c53a9c92ba2cac4dd8f0226d17780ec38edfe
Ismat <ismetsemedov@gmail.com>
2024-11-20 17:13:29 +0400

    check

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,11 +2,6 @@
 import NextAuth from 'next-auth';
 import { authOptions } from '@/lib/auth.config';
 
-const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST };
-
-
-
 const authOptions: AuthOptions = {
  providers: [
    CredentialsProvider({
