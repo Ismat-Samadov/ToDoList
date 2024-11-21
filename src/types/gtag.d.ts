c726ef4de72241ea6323488e8f62d6e82888a21c
Ismat <ismetsemedov@gmail.com>
2024-11-21 19:36:34 +0400

    check

---
+++ src/types/gtag.d.ts
@@ -0,0 +1,12 @@
+interface Window {
+    gtag: (
+      type: string,
+      trackingId: string,
+      config: {
+        page_path?: string;
+        event_category?: string;
+        event_label?: string;
+        value?: number;
+      }
+    ) => void;
+  }
